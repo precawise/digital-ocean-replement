f00b1d808473e257c0d0513cb434b9c20559df88
Your_Name <Your-Email>
2020-10-03 17:14:49 +0100

    first commit

---
+++ js-script.js
@@ -0,0 +1,14 @@
+// var s = document.createElement("script");
+
+// //Check if viewport is smaller than 768 pixels
+// if(window.innerWidth < 768) {
+//     s.type = "text/css";
+//     s.src = "mobile.css";
+// }else { //Else we have a larger screen
+//     s.type = "text/css";
+//     s.src = "desktop.css";
+// }
+
+// $(function(){
+//     $("head").append(s); //Inject stylesheet
+// })
